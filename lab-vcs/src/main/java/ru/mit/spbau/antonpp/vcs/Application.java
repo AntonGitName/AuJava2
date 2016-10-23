dafa1be755986f5b3435feef765c85affde85990
Anton Mordberg <mordbergak@gmail.com>
2016-10-23 17:27:45 +0300

    add gradle

---
+++ lab-vcs/src/main/java/ru/mit/spbau/antonpp/vcs/Application.java
@@ -0,0 +1,17 @@
+package ru.mit.spbau.antonpp.vcs;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @author Anton Mordberg
+ * @since 23.10.16
+ */
+public class Application {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(Application.class);
+
+    public static void main(String[] args) {
+        LOGGER.debug("Hello, world!");
+    }
+}
