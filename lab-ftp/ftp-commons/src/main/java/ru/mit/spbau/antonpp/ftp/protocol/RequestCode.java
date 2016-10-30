6149a7f61642071368eb0e252e9b1584cfa709a1
Anton <mordbergak@gmail.com>
2016-10-30 22:18:03 +0300

    Request codes updated

--- lab-ftp/ftp-commons/src/main/java/ru/mit/spbau/antonpp/ftp/protocol/RequestCode.java
+++ lab-ftp/ftp-commons/src/main/java/ru/mit/spbau/antonpp/ftp/protocol/RequestCode.java
@@ -5,8 +5,8 @@ package ru.mit.spbau.antonpp.ftp.protocol;
  * @since 29/10/2016
  */
 public class RequestCode {
-    public static final int RQ_GET = 1;
-    public static final int RQ_LIST = 2;
+    public static final int RQ_LIST = 1;
+    public static final int RQ_GET = 2;
     public static final int RQ_DISCONNECT = 4;
 
     private RequestCode() {
